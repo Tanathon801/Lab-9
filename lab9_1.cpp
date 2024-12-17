4d6fb9b0010004d77a4885faaa258e40b542121a
Tanathon801 <aumatomic@gmail.com>
2024-12-17 15:12:50 +0700

    Update lab9_1_ddg.cpp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,10 +2,17 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double x;
+    double z;
+    double c;
+    
 	cout << "Enter initial loan: ";
+	cin >> x;
 	cout << "Enter interest rate per year (%): ";
+	cin >> z;
 	cout << "Enter amount you can pay per year: ";
+	cin >> c; 
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -17,17 +24,34 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	double i;
+	int t = 1;
+	i = x;
+	do {
+	double o; 
+	o = i;
 	
+	double y;
+	y = o*(z/100);
+	double u;
+	u = o+y;
+	if(u < c){
+	   c = u;
+	}
+	i = u-c;
+
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << t; 
+	cout << setw(13) << left << o;
+	cout << setw(13) << left << y;
+	cout << setw(13) << left << u;
+	cout << setw(13) << left << c;
+	cout << setw(13) << left << i;
 	cout << "\n";	
-	
+		o = i;
+		t++;
+	} while (i != 0);
 	return 0;
 }
