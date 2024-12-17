bd9c80c2b967be4975d56e3d4d6a37c39c4b19c5
Tanathon801 <aumatomic@gmail.com>
2024-12-17 15:13:40 +0700

    Update lab9_2_fdd.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,22 +3,17 @@ using namespace std;
 
 //Write the function printO() here
 
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+int printO(int n,int m){
+    if(n > 0 && m >0){
+        for(int x=0;x<n;x++){
+            for(int y=0;y<m;y++){
+                cout << "O";
+            }
+        cout << "\n";
+        }
+    }
+    else{
+        cout << "Invalid input";
+    }
+    return 0;
 }
